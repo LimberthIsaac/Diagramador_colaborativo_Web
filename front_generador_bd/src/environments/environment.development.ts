7d4732deda26f4eacf2652f8ebe3456511af23cc
LimberthIsaac <134825853+LimberthIsaac@users.noreply.github.com>
2026-09-16 12:27:40 -0400

    fix:correcion de enlaces de direccionamiento

--- front_generador_bd/src/environments/environment.development.ts
+++ front_generador_bd/src/environments/environment.development.ts
@@ -11,8 +11,11 @@ export const environment = {
     // endpoint_java: `http://127.0.0.1:${portJava}/`,
 
     // ENLACES DE PRODUCCIÓN (Railway)
-    endpoint_python: `https://diagramadorumlcolaborativo-production.up.railway.app/`,
-    WebSocket_python: `diagramadorumlcolaborativo-production.up.railway.app`,
-    endpoint_java: `https://extraordinary-alignment-production.up.railway.app/`
+    // endpoint_python: `https://diagramadorumlcolaborativo-production.up.railway.app/`,
+    endpoint_python: `https://diagramadorcolaborativouml-production.up.railway.app/`,
+    // WebSocket_python: `diagramadorumlcolaborativo-production.up.railway.app`,
+    WebSocket_python: `diagramadorcolaborativouml-production.up.railway.app`,
+    // endpoint_java: `https://extraordinary-alignment-production.up.railway.app/`
+    endpoint_java: `https://generadorbackendspringboot-production.up.railway.app/`
 };
 
